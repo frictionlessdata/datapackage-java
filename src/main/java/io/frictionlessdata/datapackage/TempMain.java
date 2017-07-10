1cde9d8be8422d183f74bc3f5623ce2f9b51aa36
Georges Labrèche <georges@opendatakosovo.org>
2017-07-11 00:40:43 +0200

    fixed basicschema validation testing

--- src/main/java/io/frictionlessdata/datapackage/TempMain.java
+++ src/main/java/io/frictionlessdata/datapackage/TempMain.java
@@ -1,3 +1,8 @@
+
+import io.frictionlessdata.datapackage.Validator;
+import org.everit.json.schema.ValidationException;
+import org.json.JSONObject;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -16,6 +21,12 @@ public class TempMain {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Initial setup of library.");
+        
+        /**
+        JSONObject datapackageJsonObject = new JSONObject("{\"invalid\" : \"json\"}");
+        
+        Validator validator = new Validator();
+        validator.validate(datapackageJsonObject);**/
     }
     
 }
--- src/main/java/io/frictionlessdata/datapackage/Validator.java
+++ src/main/java/io/frictionlessdata/datapackage/Validator.java
@@ -11,17 +11,13 @@ import org.json.JSONTokener;
  * Validates against schema.
  */
 public class Validator {
-    
-    //FIXME: File should most likely reside and be loaded locally instead.
-    private final String SCHEMA_DATA_PACKAGE_JSON = "http://specs.frictionlessdata.io/schemas/data-package.json";
-    
     /**
      * Validates a given JSON Object against the datapackage.json schema.
      * @param datapackageJsonObject
      * @throws ValidationException 
      */
     public void validate(JSONObject datapackageJsonObject) throws ValidationException{
-        InputStream inputStream = getClass().getResourceAsStream(SCHEMA_DATA_PACKAGE_JSON);
+        InputStream inputStream = Validator.class.getResourceAsStream("/schemas/data-package.json");
         JSONObject rawSchema = new JSONObject(new JSONTokener(inputStream));
         Schema schema = SchemaLoader.load(rawSchema);
         schema.validate(datapackageJsonObject); // throws a ValidationException if this object is invalid
