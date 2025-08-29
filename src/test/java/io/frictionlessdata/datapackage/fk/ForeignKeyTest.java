0bf1f7ec2969b5f1ddaf0187452d690690e1d45c
JanderJ1 <johannes.jander@basf.com>
2025-08-29 19:26:39 +0200

    Fix for https://github.com/frictionlessdata/datapackage-java/issues/63 - null values in FK field

--- src/test/java/io/frictionlessdata/datapackage/fk/ForeignKeyTest.java
+++ src/test/java/io/frictionlessdata/datapackage/fk/ForeignKeyTest.java
@@ -1,5 +1,7 @@
-package io.frictionlessdata.datapackage;
+package io.frictionlessdata.datapackage.fk;
 
+import io.frictionlessdata.datapackage.Package;
+import io.frictionlessdata.datapackage.TestUtil;
 import io.frictionlessdata.datapackage.exceptions.DataPackageValidationException;
 import io.frictionlessdata.datapackage.resource.Resource;
 import io.frictionlessdata.tableschema.exception.ForeignKeyException;
@@ -8,24 +10,26 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.nio.file.Path;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class ForeignKeysTest {
+public class ForeignKeyTest {
 
     @Test
     @DisplayName("Test that foreign keys are validated correctly, good case")
     void testForeignKeysGoodCase() throws Exception{
         Path resourcePath = TestUtil.getResourcePath("/fixtures/datapackages/foreign_keys_valid.json");
-        Package pkg = new Package(resourcePath, true);
-        pkg.getResource("teams");
+        io.frictionlessdata.datapackage.Package pkg = new io.frictionlessdata.datapackage.Package(resourcePath, true);
+        Resource teams = pkg.getResource("teams");
+        teams.checkRelations(pkg);
     }
 
     @Test
     @DisplayName("Test that foreign keys are validated correctly, bad case")
     void testForeignKeysBadCase() throws Exception{
         Path resourcePath = TestUtil.getResourcePath("/fixtures/datapackages/foreign_keys_invalid.json");
-        Package pkg = new Package(resourcePath, true);
+        io.frictionlessdata.datapackage.Package pkg = new Package(resourcePath, true);
         Resource teams = pkg.getResource("teams");
 
         DataPackageValidationException ex = assertThrows(DataPackageValidationException.class,
@@ -33,5 +37,18 @@ public class ForeignKeysTest {
         Throwable cause = ex.getCause();
         Assertions.assertInstanceOf(ForeignKeyException.class, cause);
         Assertions.assertEquals("Foreign key validation failed: [city] -> [name]: 'Munich' not found in resource 'cities'.", cause.getMessage());
+    }
+
+    @Test
+    @DisplayName("Test checkRelations on valid resources in different-valid-data-formats datapackage")
+    void testCheckRelationsOnAllResources() throws Exception {
+        Path resourcePath = TestUtil.getResourcePath("/fixtures/datapackages/different-valid-data-formats/datapackage.json");
+        Package dp = new Package(resourcePath, true);
+
+        dp.getResource("teams_with_headers_csv_file").checkRelations(dp);
+        dp.getResource("teams_arrays_inline").checkRelations(dp);
+        dp.getResource("teams_objects_inline").checkRelations(dp);
+        dp.getResource("teams_arrays_file").checkRelations(dp);
+        dp.getResource("teams_objects_file").checkRelations(dp);
     }
 }
